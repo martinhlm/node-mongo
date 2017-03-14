bee7554703035d64d0ce5ee6e5e219ac9c2256c6
martinhlm <martinh.mireles@gmail.com>
2017-03-14 04:13:29 -0600

    refactoring code

--- node-examples/solve-3.js
+++ node-examples/solve-3.js
@@ -5,20 +5,18 @@ var argv = require('yargs')
 
 var rect = require('./rectangle-2');
 
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = "
-                + l + " and b = " + b);
-    rect(l,b, function(err,rectangle) {
-	if (err) {
-	    console.log(err);
-	}
-	else {
+function solveRect(l, b) {
+    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+    rect(l, b, function(err, rectangle) {
+    	if (err) {
+    	    console.log(err);
+    	} else {
             console.log("The area of a rectangle of dimensions length = "
                   + l + " and breadth = " + b + " is " + rectangle.area());
-	    console.log("The perimeter of a rectangle of dimensions length = "
+    	    console.log("The perimeter of a rectangle of dimensions length = "
                   + l + " and breadth = " + b + " is " + rectangle.perimeter());
-	}
+    	}
     });
 };
 
-solveRect(argv.l,argv.b);
+solveRect(argv.l, argv.b);
