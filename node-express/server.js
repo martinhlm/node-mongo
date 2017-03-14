5ff28cc51e4cd8793eb7a829011a2f68c1d4fe7a
martinhlm <martinh.mireles@gmail.com>
2017-03-14 06:13:32 -0600

    create promoRouter module

--- node-express/server.js
+++ node-express/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 
 var dishRouter = require('./dishRouter');
+var promoRouter = require('./promoRouter');
 
 var hostname = 'localhost';
 var port = 4000;
@@ -10,6 +11,7 @@ var app = express();
 
 app.use(morgan('dev'));
 app.use('/dishes', dishRouter.createDish());
+app.use('/promotions', promoRouter.createPromo());
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port, hostname, function() {
