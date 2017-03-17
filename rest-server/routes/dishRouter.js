eda27120147d42140f073a476099a69634057fbf
martinhlm <martinh.mireles@gmail.com>
2017-03-17 16:20:17 -0600

    update get and post routers

--- rest-server/routes/dishRouter.js
+++ rest-server/routes/dishRouter.js
@@ -1,23 +1,35 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Dishes = require('../models/dishes');
 
 var dishRouter = express.Router();  // is like a mini-express application
 dishRouter.use(bodyParser.json());
 
 
 dishRouter.route('/')
-.all(function(req, res, next) {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      next();
-})
 
 .get(function(req, res, next) {
-        res.end('Will send all the dishes to you!');
+    Dishes.find({}, function (err, dish) {
+        if (err) throw err;
+
+        res.json(dish);
+    });
 })
 
 .post(function(req, res, next) {
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' +
-    req.body.description);
+    Dishes.create(req.body, function (err, dish) {
+        if (err) throw err;
+
+        console.log('Dish created');
+        var id = dish._id;
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+
+        res.end(`Added the dish wit id ${id}`);
+    });
 })
 
 .delete(function(req, res, next) {
